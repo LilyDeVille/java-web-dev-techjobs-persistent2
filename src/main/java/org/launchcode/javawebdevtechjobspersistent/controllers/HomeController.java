cf578b6781593dd83904226e6d3e0da283dc8a90
LilyDeVille <lily.deville@hotmail.com>
2021-03-25 17:18:04 -0500

    Finished assignment.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -1,6 +1,12 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
 import org.launchcode.javawebdevtechjobspersistent.models.Job;
+import org.launchcode.javawebdevtechjobspersistent.models.Skill;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.JobRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -8,12 +14,22 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
  */
 @Controller
 public class HomeController {
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
 
     @RequestMapping("")
     public String index(Model model) {
@@ -26,6 +42,8 @@ public class HomeController {
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
+        model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
@@ -39,7 +57,17 @@ public class HomeController {
             return "add";
         }
 
-        return "redirect:";
+        Optional optEmployer = employerRepository.findById(employerId);
+        if (optEmployer.isPresent()) {
+            Employer employer = (Employer) optEmployer.get();
+            newJob.setEmployer(employer);
+        }
+
+        List<Skill> optSkill = (List<Skill>) skillRepository.findAllById(skills);
+        newJob.setSkills(optSkill);
+        jobRepository.save(newJob);
+
+        return "redirect:/";
     }
 
     @GetMapping("view/{jobId}")
